39203706e926bfbba9a33191e0cd58bd0b89b322
JacobMJones <theretherehuman@gmail.com>
2018-05-22 16:10:34 +0000

    form counts tweet length and alerts user if tweet length is longer than 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    console.log('ready!');
 
+    $(".form_text_area").on('input',function (event) {
+        let currentText = $(this).val();
+        let counterValue = 140 - currentText.length;
+        let counter = $(this).siblings("span");
+
+        counter.text(counterValue);
+        if(counterValue<0){
+            counter.css('color', 'red');
+        }
+    });
 });
+
+
